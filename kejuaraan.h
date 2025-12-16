f2ebe658bdf1d58d44ac9ed2b305e413e1cb6578
yohannapurnomo <yohannapurnomo2005@gmail.com>
2025-12-16 16:15:02 +0700

    Add header file for Kejuaraan structure and functions

---
+++ kejuaraan.h
@@ -0,0 +1,21 @@
+#ifndef KEJUARAAN_H
+#define KEJUARAAN_H
+
+#include <iostream>
+using namespace std;
+
+struct Kejuaraan {
+    int idKejuaraan;
+    string nama;
+    Kejuaraan* next;
+};
+
+struct ListKejuaraan {
+    Kejuaraan* first;
+};
+
+void createListKejuaraan(ListKejuaraan &LK);
+void insertKejuaraan(ListKejuaraan &LK, int id, string nama);
+Kejuaraan* cariKejuaraan(ListKejuaraan LK, int id);
+
+#endif
