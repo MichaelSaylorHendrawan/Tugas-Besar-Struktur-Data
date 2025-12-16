eae14ebb1c439562ad935ebc67f4f74370517565
yohannapurnomo <yohannapurnomo2005@gmail.com>
2025-12-16 22:45:03 +0700

    Refactor pointer syntax in kejuaraan.h

--- kejuaraan.h
+++ kejuaraan.h
@@ -1,21 +1,22 @@
 #ifndef KEJUARAAN_H
 #define KEJUARAAN_H
 
-#include <iostream>
+#include <string>
 using namespace std;
 
 struct Kejuaraan {
     int idKejuaraan;
     string nama;
-    Kejuaraan* next;
+    Kejuaraan *next;
 };
 
 struct ListKejuaraan {
-    Kejuaraan* first;
+    Kejuaraan *first;
 };
 
 void createListKejuaraan(ListKejuaraan &LK);
 void insertKejuaraan(ListKejuaraan &LK, int id, string nama);
 Kejuaraan* cariKejuaraan(ListKejuaraan LK, int id);
+void hapusKejuaraan(ListKejuaraan &LK, int id);
 
 #endif
