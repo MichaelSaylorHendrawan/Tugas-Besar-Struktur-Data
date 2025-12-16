74382857acca76c74bfd7c0079e34d93e6da351f
yohannapurnomo <yohannapurnomo2005@gmail.com>
2025-12-16 22:59:09 +0700

    add komen'
    Added comments to clarify the purpose of structs and functions.

--- peserta.h
+++ peserta.h
@@ -4,21 +4,26 @@
 #include <iostream>
 using namespace std;
 
+// Menyimpan data peserta dan pointer ke peserta berikutnya
+// Pointer next menandakan bahwa struktur ini adalah linked list
 struct Peserta {
     int idPeserta;
     string nama;
     Peserta* next;
 };
 
+// Menyimpan alamat node pertama dari linked list peserta
+// Ini adalah implementasi ADT List Peserta
 struct ListPeserta {
     Peserta* first;
 };
 
+
 // prototype fungsi
-void createListPeserta(ListPeserta &LP);
-void insertPeserta(ListPeserta &LP, int id, string nama);
-Peserta* cariPeserta(ListPeserta LP, int id);
-void hapusPeserta(ListPeserta &LP, int id);
+void createListPeserta(ListPeserta &LP); // Menginisialisasi list peserta (list kosong)
+void insertPeserta(ListPeserta &LP, int id, string nama); // Menambahkan peserta baru ke dalam list
+Peserta* cariPeserta(ListPeserta LP, int id); // Mencari peserta berdasarkan id
+void hapusPeserta(ListPeserta &LP, int id); // Menghapus peserta berdasarkan id
 
 #endif
 
