3df21ff7c8982ec1ed61f04335ef4e06d7143c78
Saylor Twift <156284961+MichaelSaylorHendrawan@users.noreply.github.com>
2025-12-17 21:52:56 +0700

    Add structures for participants and relationships

--- kejuaraan.h
+++ kejuaraan.h
@@ -1,22 +1,54 @@
 #ifndef KEJUARAAN_H
 #define KEJUARAAN_H
 
+#include <iostream>
 #include <string>
 using namespace std;
+
+struct Peserta {
+    int idPeserta;
+    string nama;
+    string email;
+    int umur;
+    Peserta* next;
+    Peserta* prev;
+};
+
+struct ListPeserta {
+    Peserta* first;
+    Peserta* last;
+};
+
 
 struct Kejuaraan {
     int idKejuaraan;
     string nama;
-    Kejuaraan *next;
+    string kategori;        // Data tambahan untuk memenuhi minimal 3 data
+    int tahun;              // Data tambahan bertipe integer
+    int jumlahPeserta;
+    Kejuaraan* next;
 };
 
 struct ListKejuaraan {
-    Kejuaraan *first;
+    Kejuaraan* first;
 };
 
-void createListKejuaraan(ListKejuaraan &LK);
-void insertKejuaraan(ListKejuaraan &LK, int id, string nama);
-Kejuaraan* cariKejuaraan(ListKejuaraan LK, int id);
-void hapusKejuaraan(ListKejuaraan &LK, int id);
+struct Relasi {
+    Peserta* peserta;
+    Kejuaraan* kejuaraan;
+    string tanggalDaftar;   // Data tambahan untuk relasi
+    Relasi* next;
+};
 
-#endif
+struct ListRelasi {
+    Relasi* first;
+};
+
+// ======= PROTOTIPE FUNGSI PESERTA (DOUBLE CIRCULAR) ==========
+void createListPeserta(ListPeserta &LP);
+void insertFirstPeserta(ListPeserta &LP, int id, string nama, string email, int umur);
+void insertLastPeserta(ListPeserta &LP, int id, string nama, string email, int umur);
+void deleteFirstPeserta(ListPeserta &LP, ListRelasi &LR);
+void deleteLastPeserta(ListPeserta &LP, ListRelasi &LR);
+Peserta* cariPeserta(ListPeserta LP, int id);
+void hapusPeserta(ListPeserta &LP, int id, ListRelasi &LR);
